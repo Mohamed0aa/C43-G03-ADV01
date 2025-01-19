c31e7cec8f1071639ef05e7fb5ecb66c07e2562e
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-19 22:22:15 +0200

    v1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,20 +4,7 @@
     {
         static void Main(string[] args)
         {
-            #region vedio 1
-            //int a = 6, b = 7;
-
-            //Helper.Swap(ref a, ref b);
-
-            //Console.WriteLine($"{a},{b}");
-
-
-            //double c = 6.4, d = 7.5;
-
-            //Helper.Swap(ref c, ref d);
-
-            //Console.WriteLine($"{c},{d}"); 
-            #endregion
+           
 
 
         }
