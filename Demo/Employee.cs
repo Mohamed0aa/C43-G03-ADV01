578a8fc456e603164495ec1ef4e168b6d469d2f2
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-20 16:12:58 +0200

    v2

---
+++ Demo/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int  Salary { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            Employee? other =(Employee?) obj; //  un save custing i dont what can get from function
+            return this.Id == other?.Id && this.Name==other.Name && this.Salary == other.Salary;
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            #region vedio 1
             //int a = 6, b = 7;
 
             //Helper.Swap(ref a, ref b);
@@ -16,6 +17,23 @@
             //Helper.Swap(ref c, ref d);
 
             //Console.WriteLine($"{c},{d}"); 
+            #endregion
+
+            #region vedio 2
+            //int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, };
+            //Console.WriteLine( Helper<int>.linear_search(num,6));
+
+            //Employee[] employee =
+            // {
+            //    new Employee(){Id=20,Name="MOHAMED",Salary=20000 },
+            //    new Employee(){Id=20,Name="MOHAMED",Salary=50000 },
+            //    new Employee(){Id=20,Name="MOHAMED",Salary=60000 },
+
+            // };
+
+
+            //Console.WriteLine(Helper<Employee>.linear_search(employee, new Employee() { Id = 20, Name = "MOHAMED", Salary = 60000 }));
+            #endregion
         }
     }
 }
